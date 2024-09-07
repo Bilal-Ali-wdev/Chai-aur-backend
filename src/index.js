aa48b47054f96fcb52f18acd6e70b6b073270a57
Bilal-Ali-wdev <bilalwdev26@gmail.com>
2024-09-07 02:29:35 -0700

    Add error checking utils

--- src/index.js
+++ src/index.js
@@ -2,28 +2,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import mongoose from "mongoose";
+import express from "express"
+const app = express()
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
-
-//const app = express()
-// //ifi
-// ;(async()=>{
-//     try{
-//          await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//          app.on("error",(error)=>{
-//             console.log("ERROR : ",error);
-//             throw error
-//          })
-//          app.listen(process.env.PORT,()=>{
-//             console.log(`App is listen on PORT ${process.env.PORT}`);
-//          })
-//     }
-//     catch(err){
-//         console.error("ERROR : ",err)
-//         throw err
-//     }
-// })()
+connectDB().then(()=>{
+   app.listen(process.env.PORT || 8001 , ()=>{
+    console.log(`Server is running at PORT : ${process.env.PORT}`);
+   }) 
+}).catch((err)=>{
+  console.log("MONGODB CONNECTION FAILED : ",err);
+});
