f70fc6eeecd1bde2ac119ae2e00231271338b1a4
Bilal-Ali-wdev <bilalwdev26@gmail.com>
2024-09-08 14:36:38 -0700

    add multer for upcoming files

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"; //filesystem
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //if file exist upload on cloudinary
+    const res = await cloudinary.uploader.upload(localFilePath, () => {
+      resource_type: "auto";
+    });
+    console.log("Response is : ", res);
+    //file has been upladed successfully
+    console.log("File is uploaded in cloudinary");
+    res.url;
+    return res;
+  } catch (error) {
+    fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the upload operation failed
+    return null
+  }
+};
+
+export {uploadOnCloudinary}
